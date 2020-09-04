ce9427ceb3d160cb475ae318bf925dafaef06205
Ray Soyama <raysoyama@gmail.com>
2020-09-04 15:52:56 -0700

    Added Basic Trigger Functionality

---
+++ UW Summer Jam/Assets/_HuskyJam/Scripts/MapController/MapManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public static MapManager instance = null;
+
+    [SerializeField]
+    private List<GameObject> trigger1Enable = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> trigger1Disable = new List<GameObject>();
+
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+    public void OnTriggerHit(string triggerName)
+    {
+        Debug.Log($"Trigger hit {triggerName}");
+
+        switch (triggerName)
+        {
+            case ("Trigger 1"):
+                EnableDisableObjects(trigger1Enable, trigger1Disable);
+
+                break;
+        }
+    }
+
+    private void EnableDisableObjects(List<GameObject> enableList, List<GameObject> disableList)
+    {
+        foreach (GameObject go in enableList)
+        {
+            go.SetActive(true);
+        }
+
+        foreach (GameObject go in disableList)
+        {
+            go.SetActive(false);
+        }
+    }
+
+}
---
+++ UW Summer Jam/Assets/_HuskyJam/Scripts/MapController/TriggerListener.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TriggerListener : MonoBehaviour
+{
+    public string triggerName = "Trigger";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") == true)
+        {
+            MapManager.instance.OnTriggerHit(triggerName);
+            gameObject.SetActive(false);
+        }
+    }
+
+}
